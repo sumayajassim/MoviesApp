46c49519da78e59a5fe47e78403fdc994ea4b9cf
sumayajassim <sumayajassim1@gmail.com>
2023-03-01 16:28:28 +0300

    change the img  src in Homepage

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -40,12 +40,12 @@ function HomePage() {
           <div className="h-96 min-w-fit rounded-md relative drop-shadow-md" key={movie.id}>
             <img
               className="h-96 w-60 object-fill rounded-md"
-              src="https://picsum.photos/200/300?grayscale"
+              src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
               alt=""
             />
             <div className="absolute bottom-0 bg-white w-60 h-20 rounded-br-md rounded-bl-md p-4 flex justify-between">
               <span className="text-red-700">{movie.title? movie.title : movie.name}</span>
-              <span>{movie.rate}</span>
+              <span>{movie.vote_average}</span>
             </div>
           </div>
       ))}
