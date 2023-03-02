a654438e3818f63b484493c562d54c41fce56b74
TaherJalal <taherjalaljuma@gmail.com>
2023-03-02 00:36:03 -0800

    changed some apis with try and catch

--- pages/api/tvshowspage.ts
+++ pages/api/tvshowspage.ts
@@ -1,22 +1,25 @@
 import { NextApiRequest , NextApiResponse } from "next";
 import axios from 'axios'
 
-export default function moviePage(req: NextApiRequest , res: NextApiResponse){
+export default async function moviePage(req: NextApiRequest , res: NextApiResponse){
  const pageNumber = req.body.page
 
  if(pageNumber && pageNumber > 0){
-  axios.get(`https://api.themoviedb.org/3/discover/tv?api_key=010b85a5594b639d99d3ea642bd45c74&language=en-US&sort_by=original_title.desc&include_adult=false&include_video=false&page=${pageNumber}&with_watch_monetization_types=flatrate`)
-    .then(data => {
-        res.json(data.data.results)
-    })
-    .catch(err => {
-        console.log(err)
-    })
+        res.json(await fetchData(pageNumber))
+    }
     
 }
-    else{
-    return
-     }
+
+async function fetchData(pageNumber:any){
+    const {data} = await axios.get(`https://api.themoviedb.org/3/discover/tv?api_key=010b85a5594b639d99d3ea642bd45c74&language=en-US&sort_by=original_title.desc&include_adult=false&include_video=false&page=${pageNumber}&with_watch_monetization_types=flatrate`)
+    
+    try {
+        return data.results
+    }
+
+    catch(err){
+        ( console.log(err))
+    }
 }
 
 
