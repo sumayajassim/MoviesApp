dc4ed823312dd6d060097e7014701dc6a25c073b
sumayajassim <sumayajassim1@gmail.com>
2023-03-06 07:55:32 +0300

    pagination is working in movies page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app'
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 import Context from "../context/context"
 export default function App({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient() 
@@ -11,6 +12,8 @@ export default function App({ Component, pageProps }: AppProps) {
     <QueryClientProvider client={queryClient}>
         <Component {...pageProps} />
         <ToastContainer />
+         <ReactQueryDevtools initialIsOpen={true} />
+
       </QueryClientProvider>
   </Context>
   </> 
--- pages/movie/index.tsx
+++ pages/movie/index.tsx
@@ -1,59 +1,83 @@
-import React , {useState} from 'react'
-import Navbar from '@/components/Navbar'
-import { useQueryClient , useQuery } from '@tanstack/react-query'
-import axios from 'axios'
-import PageNavigation from '@/components/PageNavigation';
+import React, { useState, useEffect } from "react";
+import Navbar from "@/components/Navbar";
+import { useQueryClient, useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import PageNavigation from "@/components/PageNavigation";
+import MovieComponent from "@/components/MovieComponent";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { useInView } from 'react-intersection-observer'
+function index(props: any) {
+  const queryClient = useQueryClient();
+  const { ref, inView } = useInView();
+  const [search, setSearch] = useState('')
+  const {
+    data,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+    status,
+  } = useInfiniteQuery({
+    queryKey: ["movies"],
+    queryFn : async ({ pageParam = 1 }) => {
+      const res = await axios.get('/api/moviespage?page=' + pageParam)
+      return res.data
+    },
+    getNextPageParam: (lastPage,pages) => pages.length + 1,
+    refetchOnWindowFocus: false,
+  });
 
+  useEffect(() => {
+    if (inView) {
+      fetchNextPage()
+    }
+  }, [inView])
 
-
-
-function index(props:any) {
-
-  const [page , setPage] = useState(1)
-
-const queryClient = useQueryClient();
-
-const {error , isLoading , data} = useQuery<Data>({
-  queryKey: ['movies'],
-  queryFn: async () => await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=010b85a5594b639d99d3ea642bd45c74&language=en-US&sort_by=original_title.asc&include_adult=false&include_video=false&page=${page}&with_watch_monetization_types=flatrate`)
-})
-
-console.log(data)
-
-if (isLoading) return 'Loading...'
-
-if (error) return 'An error has occurred: ' + error?.message
+  console.log("pages", data);
+  
+  const changeHandler = (e:any) => {
+    setSearch(search => e.target.value)
+    console.log(search)
+  }
 
   return (
-    
     <div>
-      <Navbar isAuth={props.isAuth}/>
-      <h1 className='text-6xl text-red-700 px-3 py-2'>All Movies</h1>
-      <div className='grid grid-rows-5 grid-flow-col-dense gap-20 justify-center'>
-        {data?.data?.results?.map((movies:any) => (
-          <>
-          <div className='flex flex-col h-72 w-60 gap-y-2 gap-x-2 bg-red-900 m-3 shadow-xl rounded'>
-            <img className='rounded bg-fit h-80 w-60' src={`https://image.tmdb.org/t/p/original/${movies.poster_path}`} alt="" />
-
-            <div className="absolute bottom-0 bg-white w-60 h-20 rounded-br-md rounded-bl-md p-4 flex justify-between">
-              <p>{movies.title}</p>
-            </div>
-          
-            
-              {/* <p>{movies.id}</p>
-              <p>{movies.release_date}</p> */}
+      <Navbar isAuth={props.isAuth} />
+      <div>
+          <input type="text" name="searchBar" id="searchBar" onChange={changeHandler} />
+        <div className="grid grid-cols-4 gap-4 p-10 max-w-fit mx-auto my-0 justify-center">
+          {data?.pages?.map((page) => (
+            <React.Fragment key={page.page}>
+              {page?.results?.map((movie) => (
+                <MovieComponent movie={movie} />
+              ))}
+            </React.Fragment>
+          ))}
+          <div>
+            <button
+              ref={ref}
+              onClick={() => fetchNextPage()}
+              disabled={!hasNextPage || isFetchingNextPage}
+            >
+              {isFetchingNextPage
+                ? 'Loading more...'
+                : hasNextPage
+                ? 'Load Newer'
+                : 'Nothing more to load'}
+            </button>
           </div>
-          </>
-        ))}
-      </div>
-      <br />
-      <div className='flex w-screen justify-center align-center gap-7 py-4'>
-        <p className='text-red-900 p-1 cursor-pointer' onClick={() => setPage(page-1)}>Previos Page</p>
-        <p className='text-red-900 p-1 cursor-default'>Page {page}</p>
-        <p className='text-red-900 p-1 cursor-pointer' onClick={() => setPage(page+1)}>Next Page</p>
+          <div>
+            {isFetching && !isFetchingNextPage
+              ? 'Background Updating...'
+              : null}
+          </div>
+        </div>
     </div>
     </div>
-  )
+    
+  );
 }
 
-export default index
+export default index;
