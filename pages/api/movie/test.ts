488db0a58655903542d1e39c306ea07320a86c96
t4h3r <taherjalaljuma@gmail.com>
2023-03-14 01:04:31 -0700

    added isPurchased & inCart & inWishlist in movies page

--- pages/api/movie/test.ts
+++ pages/api/movie/test.ts
@@ -59,47 +59,46 @@ export default async function test(req: NextApiRequest, res: NextApiResponse) {
   // authorized
 
   const userDetails: any = jwtDecode(token as string);
-  //   console.log(userDetails);
 
-  //   const { data } = await axios.get(
-  //     `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=original_title.asc&include_adult=false&include_video=false&page=${pageNumber}&with_watch_monetization_types=flatrate&with_genres=${genreId}`
-  //   );
+  const { data } = await axios.get(
+    `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=original_title.asc&include_adult=false&include_video=false&page=${pageNumber}&with_watch_monetization_types=flatrate&with_genres=${genreId}`
+  );
 
-  //   const movieIdsArray = data.results.map(
-  //     (movie: string, index: any) => data.results[index].id
-  //   );
+  const movieIdsArray = data.results.map(
+    (movie: string, index: any) => data.results[index].id
+  );
 
-  //   const trendingMovies = await axios.get(
-  //     "https://api.themoviedb.org/3/discover/movie?api_key=010b85a5594b639d99d3ea642bd45c74&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1"
-  //   );
+  const trendingMovies = await axios.get(
+    "https://api.themoviedb.org/3/discover/movie?api_key=010b85a5594b639d99d3ea642bd45c74&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1"
+  );
 
-  //   const trendingMoviesArray = data.results.map(
-  //     (movie: string, index: any) => trendingMovies.data.results[index].id
-  //   );
+  const trendingMoviesArray = data.results.map(
+    (movie: string, index: any) => trendingMovies.data.results[index].id
+  );
 
-  //   const topRatedMovies = await axios.get(
-  //     `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`
-  //   );
+  const topRatedMovies = await axios.get(
+    `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`
+  );
 
-  //   const topRatedMoviesArray = data.results.map(
-  //     (movie: string, index: any) => topRatedMovies.data.results[index].id
-  //   );
+  const topRatedMoviesArray = data.results.map(
+    (movie: string, index: any) => topRatedMovies.data.results[index].id
+  );
 
-  //   const UpcomingMovies = await axios.get(
-  //     `https://api.themoviedb.org/3/movie/upcoming?api_key=010b85a5594b639d99d3ea642bd45c74&language=en-US&page=1`
-  //   );
+  const UpcomingMovies = await axios.get(
+    `https://api.themoviedb.org/3/movie/upcoming?api_key=010b85a5594b639d99d3ea642bd45c74&language=en-US&page=1`
+  );
 
-  //   const UpcomingMoviesArray = data.results.map(
-  //     (movie: string, index: any) => UpcomingMovies.data.results[index].id
-  //   );
+  const UpcomingMoviesArray = data.results.map(
+    (movie: string, index: any) => UpcomingMovies.data.results[index].id
+  );
 
-  //   movieIdsArray.map((movie: string, index: any) => {
-  //     trendingMoviesArray.includes(movie) ||
-  //     topRatedMoviesArray.includes(movie) ||
-  //     UpcomingMoviesArray.includes(movie)
-  //       ? (data.results[index].price = 10)
-  //       : (data.results[index].price = 5);
-  //   });
+  movieIdsArray.map((movie: string, index: any) => {
+    trendingMoviesArray.includes(movie) ||
+    topRatedMoviesArray.includes(movie) ||
+    UpcomingMoviesArray.includes(movie)
+      ? (data.results[index].price = 10)
+      : (data.results[index].price = 5);
+  });
 
   const purchases = await prisma.purchases.findMany({
     where: {
@@ -107,24 +106,43 @@ export default async function test(req: NextApiRequest, res: NextApiResponse) {
     },
   });
 
-  purchases.flatMap((x) => x.moviesIDs);
-  console.log(purchases);
+  const purchasedMovies = purchases
+    .map((x: any, index: number) => x.moviesIDs)
+    .flatMap((x) => x);
 
-  //   const cart = await prisma.cart.findUniqueOrThrow({
-  //     where: {
-  //       userID: userDetails.user.id,
-  //     },
-  //   });
+  const cart = await prisma.cart.findUniqueOrThrow({
+    where: {
+      userID: userDetails.user.id,
+    },
+  });
 
-  //  const userCart = cart.moviesIDs
+  const userCart = cart.moviesIDs;
 
-  //  const wishlist = await prisma.wishlist.findUniqueOrThrow({
-  //     where:{
-  //         userID: userDetails.user.id
-  //     }
-  //  })
+  const wishlist = await prisma.wishlist.findUniqueOrThrow({
+    where: {
+      userID: userDetails.user.id,
+    },
+  });
 
-  //  const userWishlist = wishlist.moviesIDs
+  const userWishlist = wishlist.moviesIDs;
 
-  //   res.json(data.results);
+  movieIdsArray.map((movie: any, index: number) => {
+    purchasedMovies.includes(movie)
+      ? (data.results[index].isPurchased = true)
+      : (data.results[index].isPurchased = false);
+  });
+
+  movieIdsArray.map((movie: any, index: number) => {
+    userCart.includes(movie)
+      ? (data.results[index].inCart = true)
+      : (data.results[index].inCart = false);
+  });
+
+  movieIdsArray.map((movie: any, index: number) => {
+    userWishlist.includes(movie)
+      ? (data.results[index].inWishlist = true)
+      : (data.results[index].inWishlist = false);
+  });
+
+  res.json(data);
 }
