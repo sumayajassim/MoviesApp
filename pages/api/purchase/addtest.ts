dee2f18770bcd2ba621980170a89213f8c1be49f
t4h3r <taherjalaljuma@gmail.com>
2023-03-20 02:04:04 -0700

    added remove movies from wishlist when purchased

--- pages/api/purchase/addtest.ts
+++ pages/api/purchase/addtest.ts
@@ -4,8 +4,10 @@ import jwtDecode from "jwt-decode";
 import getMovie from "@/components/helpers/getmovie";
 import axios from "axios";
 
-export default async function addtest(req: NextApiRequest , res: NextApiResponse){
-
+export default async function addtest(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const API_KEY = process.env.API_KEY;
 
   if (!req.headers["authorization"]) {
@@ -14,24 +16,26 @@ export default async function addtest(req: NextApiRequest , res: NextApiResponse
 
   const userDetails: any = jwtDecode(req.headers["authorization"] as string);
 
-  const { purchases, balance } = await prisma.user.findUniqueOrThrow({
+  const { purchases, balance, wishlist } = await prisma.user.findUniqueOrThrow({
     where: {
       id: userDetails.user.id,
     },
     include: {
       purchases: true,
+      wishlist: true,
     },
   });
 
-  let cartPrice = 0
+  let cartPrice = 0;
 
   const cart = await prisma.cart.findUniqueOrThrow({
-    where:{
-        userID: userDetails.user.id
-    }
-  })
+    where: {
+      userID: userDetails.user.id,
+    },
+  });
 
-  const cartMovies = cart.moviesIDs
+  const cartMovies = cart.moviesIDs;
+  const wishlistMovies = wishlist?.moviesIDs;
 
   const trendingMovies = await axios.get(
     "https://api.themoviedb.org/3/discover/movie?api_key=010b85a5594b639d99d3ea642bd45c74&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1"
@@ -57,7 +61,6 @@ export default async function addtest(req: NextApiRequest , res: NextApiResponse
     (id: any) => id.id
   );
 
-
   cartMovies.map((id: any) => {
     trendingMoviesArray.includes(id * 1) ||
     upcomingMoviesArray.includes(id * 1) ||
@@ -66,60 +69,102 @@ export default async function addtest(req: NextApiRequest , res: NextApiResponse
       : (cartPrice = cartPrice + 5);
   });
 
-  let discount = 0
+  let discount = 0;
+  const codeSent = req.body.code || null;
 
-  if(req.body.code){
-    const discountCode = await prisma.discount.findFirstOrThrow({
-        where:{
-            code: req.body.code
-        }
-    })
+  const discountCodes = await prisma.discount.findMany();
 
-    if(discountCode){
-        discount = (discountCode.amount / 100) * cartPrice
-    }
+  const allCodes = discountCodes.map((x: any) => x.code);
 
+  let percentage = 0;
+  let purchasedMovies: string | any[] = [];
+
+  if (purchases.length > 0) {
+    purchasedMovies = purchases
+      .map((movie: any) => movie.moviesIDs)
+      .flatMap((x: any) => x);
   }
 
+  console.log(purchasedMovies);
 
- if(req.body.confirm == false){
-    res.json({
-        message: `Your Total Will Be ${
-          Math.floor(cartPrice - discount)
-        } and your balance is ${balance}`,
+  if (req.body.code) {
+    if (allCodes.includes(req.body.code)) {
+      const discountCode = await prisma.discount.findFirstOrThrow({
+        where: {
+          code: codeSent,
+        },
       });
- }
 
- else if(req.body.confirm == true){
+      percentage = discountCode.amount;
+      discount = (discountCode.amount / 100) * cartPrice;
+    } else {
+      res.status(401).json({ message: "Invalid Discount Code" });
+      discount = 0;
+    }
+  }
+
+  if (req.body.confirm == false) {
+    res.json({
+      total: Math.floor(cartPrice - discount),
+      balance: balance,
+      discountPercentage: percentage,
+    });
+  } else if (req.body.confirm == true) {
     const makePurchase = await prisma.purchases.create({
-    data:{
+      data: {
         moviesIDs: cartMovies,
         amount: Math.floor(cartPrice - discount),
-        user:{
-            connect:{
-                id: userDetails.user.id
-            }
-        }
-    }
-  })
+        user: {
+          connect: {
+            id: userDetails.user.id,
+          },
+        },
+      },
+    });
 
-  const removeFromCart = await prisma.cart.update({
-    where:{
-        userID: userDetails.user.id
-    },
-    data:{
-        moviesIDs : []
-    }
-  }) 
-  
-  res.json({
-    message: `Your Total Will Be ${
-      Math.floor(cartPrice - discount)
-    } and your balance is ${balance}`,
-    makePurchase,
-    removeFromCart
-  });
+    const removeFromCart = await prisma.cart.update({
+      where: {
+        userID: userDetails.user.id,
+      },
+      data: {
+        moviesIDs: [],
+      },
+    });
 
- }
+    await prisma.user.update({
+      where: {
+        id: userDetails.user.id,
+      },
+      data: {
+        balance: balance - Math.floor(cartPrice - discount),
+      },
+    });
 
+    const updatedWishlist = wishlistMovies?.filter(
+      (x: any) => !purchasedMovies?.includes(x)
+    );
+
+    res.json({
+      cart: cartMovies,
+      wishlist: wishlistMovies,
+      updatedWishlist: updatedWishlist,
+    });
+
+    await prisma.wishlist.update({
+      where: {
+        userID: userDetails.user.id,
+      },
+      data: {
+        moviesIDs: updatedWishlist,
+      },
+    });
+
+    res.json({
+      balance: balance,
+      total: balance - Math.floor(cartPrice - discount),
+      discountPercentage: percentage,
+      makePurchase,
+      removeFromCart,
+    });
+  }
 }
