b3fd640494e0f192dd9eb8a293f2f9cd09890c54
t4h3r <taherjalaljuma@gmail.com>
2023-03-26 22:29:44 -0700

    cleaned signup api

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -10,94 +10,77 @@ export default async function signup(
   const SECRET_KEY = process.env.SECRET_KEY;
   if (!SECRET_KEY) throw Error("Secret key is not provided!");
 
-  function validFirstName() {
-    if (req.body.firstName.length > 3 && req.body.firstName.length < 25) {
-      return true;
-    } else {
-      res.json({
-        message: "First Name Characters Must Be More Than 3 And Less Than 25",
-      });
-    }
+  if (req.method !== "POST") {
+    res.status(401).send("Not A POST Request");
   }
 
-  function validlastName() {
-    if (req.body.lastName.length > 3 && req.body.lastName.length < 25) {
-      return true;
-    } else {
-      res.json({
-        message: "Last Name Characters Must Be More Than 3 And Less Than 25",
-      });
-    }
+  const { firstName, lastName, emailAddress, password } = req.body;
+  const salt = parseInt(process.env.SALT);
+  console.log(salt);
+  const validFirstName = firstName.length >= 3 && firstName.length <= 25;
+  const validLastName = lastName.length >= 3 && lastName.length <= 25;
+  const validEmail = emailAddress.includes("@") && emailAddress.includes(".");
+
+  const validPassword =
+    password.length >= 8 &&
+    password.length <= 22 &&
+    (password.includes("@") ||
+      password.includes(".") ||
+      password.includes("_") ||
+      password.includes("!") ||
+      password.includes("#") ||
+      password.includes("$") ||
+      password.includes("%") ||
+      password.includes("^") ||
+      password.includes("&") ||
+      password.includes("*"));
+
+  const emailExist = await prisma.user.findUnique({
+    where: {
+      emailAddress: emailAddress,
+    },
+  });
+
+  console.log(emailAddress);
+
+  if (emailExist) {
+    res.status(500).send("Email Is Already Registred");
   }
 
-  function validEmail() {
-    if (req.body.emailAddress.includes("@" && ".")) {
-      return true;
-    } else {
-      res.json({ message: "Invalid Email Addresse" });
-    }
+  if (!validFirstName) {
+    res
+      .status(400)
+      .send("First Name Characters Must Be More Than 3 And Less Than 25");
   }
 
-  const specialCharacters =
-    "@" || "." || "_" || "!" || "#" || "$" || "&" || "*" || "^";
-
-  function validPassword() {
-    if (
-      req.body.password.length > 4 &&
-      req.body.password.includes(specialCharacters)
-    ) {
-      return true;
-    } else {
-      res.json({
-        message:
-          "Password Must Conatin At Least One Special Character : @,#,$,%,&,* ... etc",
-      });
-    }
+  if (!validLastName) {
+    res
+      .status(400)
+      .send("Last Name Characters Must Be More Than 3 And Less Than 25");
   }
 
-  function valid() {
-    if (
-      validFirstName() &&
-      validlastName() &&
-      validEmail() &&
-      validPassword()
-    ) {
-      return true;
-    }
+  if (!validEmail) {
+    res.status(400).send("Invalid Email Address");
   }
 
-  if (valid()) {
-    bcrypt.hash(req.body.password, 10, function (err, hash) {
-      prisma.user
-        .create({
-          data: {
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            emailAddress: req.body.emailAddress,
-            password: hash,
-            wishlist: { create: {} },
-            cart: { create: {} },
-          },
-        })
-        .then((user: any) => {
-          const userPass = user.password;
-          const verifired = bcrypt.compare(req.body.password, userPass);
-
-          verifired.then((istrue) => {
-            if (istrue && SECRET_KEY) {
-              const token = jwt.sign({ user }, SECRET_KEY, {
-                expiresIn: 604800,
-              });
-              //  getUserDetails(data);
-              res.json({ token });
-            } else {
-              res.status(400).json({ message: "Wrong Password" });
-            }
-          });
-        })
-        .catch((err: any) => {
-          res.status(400).json({ message: "Email is already registered " });
-        });
-    });
+  if (!validPassword) {
+    res
+      .status(400)
+      .send(
+        "Password Must Conatin 8 To 22 Characters With At Least One Special Character : @,#,$,%,&,* ... etc"
+      );
   }
+
+  const hashPass = bcrypt.hashSync(password, salt);
+
+  const { id } = await prisma.user.create({
+    data: {
+      firstName,
+      lastName,
+      emailAddress,
+      password: hashPass,
+    },
+  });
+
+  res.json({ token: jwt.sign(id, SECRET_KEY) });
 }
